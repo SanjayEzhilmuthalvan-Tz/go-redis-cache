4a1d0c4379dbe2d80d0a36e5a0cc47999a57494d
Sanjai <sanjai@sanjay-inspiron-15-5518.mshome.net>
2024-03-07 12:57:20 +0530

    commit changes

--- connect.go
+++ connect.go
@@ -10,19 +10,19 @@ import (
 func main() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Password: "",
+		DB:       0,
 	})
 	ctx := context.Background()
 
-	err := client.Set(ctx, "foo", "bar", 0).Err()
+	err := client.Set(ctx, "name", "sanjay", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := client.Get(ctx, "foo").Result()
+	val, err := client.Get(ctx, "name").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("foo", val)
+	fmt.Println("name", val)
 }
